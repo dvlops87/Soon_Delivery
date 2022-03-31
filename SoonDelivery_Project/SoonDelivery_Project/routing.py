99af7180cf44cf2e740ec586082452ced8b5107c
Chae hee chan <eqholic1125@gmail.com>
2022-03-31 20:30:19 +0900

    style: footer 활성화 오류 수정

--- SoonDelivery_Project/SoonDelivery_Project/routing.py
+++ SoonDelivery_Project/SoonDelivery_Project/routing.py
@@ -1,18 +1,18 @@
-from django.urls import re_path
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from . import consumer
+# from django.urls import re_path
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from . import consumer
 
-import chat.routing
+# import chat.routing
 
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumer.ChatConsumer),
-]
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumer.ChatConsumer),
+# ]
 
-application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
