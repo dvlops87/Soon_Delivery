cbd17b2f726be4b6cc5bc0f8f364bf6ffa0fa221
Chae hee chan <eqholic1125@gmail.com>
2022-04-04 15:13:49 +0900

    feat: chat 페이지 연결 및 기본 세팅

--- SoonDelivery_Project/chat/views.py
+++ SoonDelivery_Project/chat/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def chat(request, user_id = 0):
   if user_id == 0:
     redirect('login')
   
-  return render(request, 'chat/chat_list.html')
+  return render(request, 'chat/chat.html')
 
 def room(request, room_name):
-  return render (request, 'chat/room.html', {
-    'room_name': room_name
-  })
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
